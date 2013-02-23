22aa238dc112d0dfeeb41c303f0949a6a85862d8
green0eggs <sam@cernpc>
2013-02-23 16:45:42 +0100

    Fixed bug in get_tree_from_file

--- pp/root.py
+++ pp/root.py
@@ -2,7 +2,7 @@
 import ROOT as r
 def get_tree_from_file(file_name,tree_name="DecayTree",dir_name="B02JpsiPi0Tuple/"):
   """Gets TTree out of TFile"""
-  f=r.TFile(ROOTFILES+file_name)
+  f=r.TFile(file_name)
   t=r.TTree()
   f.GetObject(dir_name+tree_name,t)
   print "Got the tree called:",t.GetName()
