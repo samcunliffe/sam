e56f2995f382dadf09d4a9efeac8c2c1a39bc1d6
sam <sam@zero-gravitas>
2015-04-24 15:11:22 +0100

    pep8

--- pp/utils/histo.py
+++ pp/utils/histo.py
@@ -1,5 +1,6 @@
 """Histogram utility functions"""
 
+
 def integer_binning(start, stop):
     nints = stop - start
     return (nints+1, start-0.5, stop+0.5)
@@ -14,6 +15,6 @@ def scale_to_largest(firstplot, secondplot):
     """fix root annoyance, increase axis first"""
     max_first, max_second = firstplot.GetMaximum(), secondplot.GetMaximum()
     if max_first < max_second:
-        delta = 0.05*(max_second-firstplot.GetMinimum()) # a bit for aesthetics
+        delta = 0.05*(max_second-firstplot.GetMinimum())  # for aesthetics
         firstplot.SetMaximum(max_second+delta)
     return firstplot, secondplot
