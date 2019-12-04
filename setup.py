12293c1ea70e2cc8808143ac5514b24d76834332
Sam Cunliffe <sam.cunliffe@desy.de>
2019-12-04 14:58:56 +0100

    Packagification.

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+    name='sam',
+    version='1.0',
+    description="Personal modules for Sam's use",
+    author="Sam Cunliffe",
+    url="https://github.com/samcunliffe/sam",
+    license='BSD',
+    install_requires = requirements,
+)
