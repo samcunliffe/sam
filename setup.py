63b909606b015562fd57ae836c97f62c7d193e65
Sam Cunliffe <sam.cunliffe@desy.de>
2019-12-04 15:07:01 +0100

    Packaging structure.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
     name='sam',
     version='1.0',
     description="Personal modules for Sam's use",
     author="Sam Cunliffe",
     url="https://github.com/samcunliffe/sam",
-    license='BSD',
-    install_requires = requirements,
+    install_requires = ['uncertainties'],
 )
